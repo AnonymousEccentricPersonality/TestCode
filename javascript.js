e69c9e153627e80f072009e4b05f731aeb1a756b
AnonymousEccentricPersonality <72965459+AnonymousEccentricPersonality@users.noreply.github.com>
2020-10-16 12:49:25 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,51 @@
+var a=0,b=0,c=0,d=0,e=0;
+function sd(){
+if(a%2==1){
+document.getElementById("jkl").style.display="none";
+document.getElementById("mll").innerHTML="+";
+}
+if(a%2==0){
+document.getElementById("jkl").style.display="inline";
+document.getElementById("mll").innerHTML="-";
+}
+}
+function af(){
+if(b%2==1){
+document.getElementById("abc").style.display="none";
+document.getElementById("ab").innerHTML="+";
+}
+if(b%2==0){
+document.getElementById("abc").style.display="inline";
+document.getElementById("ab").innerHTML="-";
+}
+}
+function klf(){
+if(c%2==1){
+document.getElementById("yui").style.display="none";
+document.getElementById("adc").innerHTML="+";
+}
+if(c%2==0){
+document.getElementById("yui").style.display="inline";
+document.getElementById("adc").innerHTML="-";
+}
+}
+function glf(){
+if(d%2==1){
+document.getElementById("mla").style.display="none";
+document.getElementById("ac").innerHTML="+";
+}
+if(d%2==0){
+document.getElementById("mla").style.display="inline";
+document.getElementById("ac").innerHTML="-";
+}
+}
+function hlf(){
+if(e%2==1){
+document.getElementById("ma").style.display="none";
+document.getElementById("ahc").innerHTML="+";
+}
+if(e%2==0){
+document.getElementById("ma").style.display="inline";
+document.getElementById("ahc").innerHTML="-";
+}
+}
